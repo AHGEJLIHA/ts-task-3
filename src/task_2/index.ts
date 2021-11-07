2b4b55b433bbba588d2195ffeef6af8391bdcdcd
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-11-07 10:48:56 +0500

    Task 2 is solved

--- src/task_2/index.ts
+++ src/task_2/index.ts
@@ -5,5 +5,52 @@
  * XRP
  * Etherium
  * Gold
-*/
+ */
+import {Currency, CurrencyType} from "../task_1";
+
+export class Dollar extends Currency {
+    private readonly _currencyType: CurrencyType;
+
+    constructor(value: number) {
+        super("Dollar", value, "USD");
+        this._currencyType = CurrencyType.materialCurrency;
+    }
+}
+
+export class Ruble extends Currency {
+    private readonly _currencyType: CurrencyType;
+
+    constructor(value: number) {
+        super("Ruble", value, "RUB");
+        this._currencyType = CurrencyType.materialCurrency;
+    }
+}
+
+export class XRP extends Currency {
+    private readonly _currencyType: CurrencyType;
+
+    constructor(value: number) {
+        super("XRP", value, "XRP");
+        this._currencyType = CurrencyType.CryptoСurrency
+    }
+}
+
+export class Etherium extends Currency {
+    private readonly _currencyType: CurrencyType;
+
+    constructor(value: number) {
+        super("Etherium", value, "ETH");
+        this._currencyType = CurrencyType.CryptoСurrency;
+    }
+}
+
+export class Gold extends Currency {
+    private readonly _currencyType: CurrencyType;
+
+    constructor(value: number) {
+        super("Gold", value, "gram");
+        this._currencyType = CurrencyType.MetalDepositCurrency;
+    }
+}
+
 
